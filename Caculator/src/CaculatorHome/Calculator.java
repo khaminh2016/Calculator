564210fc3b389c6dc863dcff65ccfd479ec78998
khaminh2016 <minhnguyenwork2016@gmail.com>
2016-11-15 17:43:40 +0700

    add menuProgramer but not complete

--- Caculator/src/CaculatorHome/Calculator.java
+++ Caculator/src/CaculatorHome/Calculator.java
@@ -32,10 +32,13 @@ import org.omg.CORBA.CTX_RESTRICT_SCOPE;
  *
  */
 public class Calculator extends JFrame {
-	int MS,MR,MC,bDele;
+	int MS, MR, MC, bDele;
 	int x = 0, y = 50, w = 45, h = 45, d = 5;
+	int x2=0,y2=100;
+	int x3=0,y3=200 ;
 	JPanel penStandard = new JPanel();
 	JPanel penScientific = new JPanel();
+	JPanel penProGrammer = new JPanel();
 	JPanel penSub = new JPanel();
 	String[][] btn = { { "MC", "MR", "MS", "M+", "M-" }, { "<--", "CE", "C", "∓", "√" }, { "7", "8", "9", "/", "%" },
 			{ "4", "5", "6", "*", "1/x" }, { "1", "2", "3", "-", "=" }, { "0", "he", ",", "+", " " }, };
@@ -44,11 +47,13 @@ public class Calculator extends JFrame {
 			{ "F-E", "Exp", "Mod", "log", "10x" }, };
 	JButton[][] btnBut = new JButton[6][5];
 	JButton[][] btnBut2 = new JButton[5][5];
+	JButton[][] btnBut3 = new JButton[6][5];
 	JMenuBar mnBar;
 	JMenu mneView, mneEdit, mneHelp;
 	JMenuItem mniStan, mniScien, mniPro, mniExit;
 	JTextField txtSrc;
 	JTextField txtSrc2;
+	JTextField txtSrc3;
 	JRadioButton bgDeg, bgRa, bgGra;
 	ButtonGroup bgSelect = new ButtonGroup();
 	int count = 0;
@@ -59,11 +64,41 @@ public class Calculator extends JFrame {
 		setSize(400, 450);
 		setTitle("Calculator");
 		setLayout(null);
-		MenuMinhStandrad();
 		MenuMinhScientific();
-		ActionBtnMinhStandard();
-		penScientific.setVisible(false);
+		ActionMenu();
+
 	}
+
+	public void ActionMenu() {
+		ActionListener bAcMeo = new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent g) {
+				// TODO Auto-generated method stub
+				if (g.getSource() == mniStan) {
+					MenuMinhStandrad();
+					ActionBtnMinhStandard();
+					penScientific.setVisible(false);
+					penStandard.setVisible(true);
+					penProGrammer.setVisible(false);
+				} else if (g.getSource() == mniScien) {
+					MenuMinhScientific();
+					penScientific.setVisible(true);
+					penStandard.setVisible(false);
+					penProGrammer.setVisible(false);
+				}else if(g.getSource()==mniPro){
+					MenuProGrammer();
+					penProGrammer.setVisible(true);
+					penStandard.setVisible(false);
+					penScientific.setVisible(false);
+				}
+			}
+		};
+		mniScien.addActionListener(bAcMeo);
+		mniStan.addActionListener(bAcMeo);
+		mniPro.addActionListener(bAcMeo);
+	}
+
 	public void MenuMinhStandrad() {
 
 		txtSrc = new JTextField();
@@ -71,8 +106,9 @@ public class Calculator extends JFrame {
 		txtSrc.setBounds(20, 10, 250, 60);
 		txtSrc.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
 		txtSrc.setHorizontalAlignment(JTextField.RIGHT);
+		txtSrc.setEnabled(false);
 		Font f = txtSrc.getFont();
-		txtSrc.setFont(new Font(f.getName(),Font.BOLD,f.getSize()+20));
+		txtSrc.setFont(new Font(f.getName(), Font.BOLD, f.getSize() + 20));
 		txtSrc.setEnabled(false);
 		mnBar = new JMenuBar();
 		mneView = new JMenu("View");
@@ -127,113 +163,178 @@ public class Calculator extends JFrame {
 		});
 		mniExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
 	}
-	// FIXME chinh loi cua boder bg
-	 public void MenuMinhScientific() {
-	 txtSrc2 = new JTextField();
-	 penScientific.add(txtSrc2);
-	 txtSrc2.setBounds(10, 10, 500, 60);
-	 txtSrc2.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
-	 txtSrc2.setEnabled(false);
-	 mnBar = new JMenuBar();
-	 mneView = new JMenu("View");
-	 mneEdit = new JMenu("Edit");
-	 mneHelp = new JMenu("Help");
-	 mniStan = new JMenuItem("Standard");
-	 mniScien = new JMenuItem("Scientific");
-	 mniPro = new JMenuItem("Programmer");
-	 mniExit = new JMenuItem("Exit");
-	 mneView.add(mniStan);
-	 mneView.addSeparator();
-	 mneView.add(mniScien);
-	 mneView.addSeparator();
-	 mneView.add(mniPro);
-	 mneView.addSeparator();
-	 mneView.add(mniExit);
-	 mnBar.add(mneView);
-	 mnBar.add(mneEdit);
-	 mnBar.add(mneHelp);
-	 setJMenuBar(mnBar);
-	 mneView.setMnemonic(KeyEvent.VK_V);
-	 mneEdit.setMnemonic(KeyEvent.VK_E);
-	 mneHelp.setMnemonic(KeyEvent.VK_H);
-	 penScientific.setLayout(null);
-	 penSub.setLayout(null);
-	 y=0;
-	 Insets s = new Insets(1, 1, 1, 1);
-	 for (int i = 0; i < 6; i++) {
-	 x = 260;
-	 for (int j = 0; j < 5; j++) {
-	 btnBut[i][j] = new JButton(btn[i][j]);
-	 penScientific.add(btnBut[i][j]);
-	 btnBut[i][j].setBounds(x, y, w, h);
-	 btnBut[i][j].setMargin(s);
-	
-	 x = x + d + w;
-	 }
-	 y = y + d + w;
-	 }
-	 y=w+d;
-	 for (int i = 0; i < 5; i++) {
-	 x=10;
-	 for (int j = 0; j < 5; j++) {
-	 btnBut2[i][j] =new JButton(btn2[i][j]);
-	 penScientific.add(btnBut2[i][j]);
-	 btnBut2[i][j].setBounds(x,y,w,h);
-	 btnBut2[i][j].setMargin(s);
-	 x=x+w+d;
-	 }
-	 y = y + d + w;
-	 }
-	 bgDeg = new JRadioButton("Degrees");
-	 bgRa = new JRadioButton("Radians");
-	 bgGra = new JRadioButton("Grads");
-	 bgSelect.add(bgDeg);
-	 bgSelect.add(bgGra);
-	 bgSelect.add(bgRa);
-	 penSub.add(bgDeg);
-	 penSub.add(bgRa);
-	 penSub.add(bgGra);
-	 bgDeg.setBounds(10,10,80,30);
-	 bgRa.setBounds(90,10,80,30);
-	 bgGra.setBounds(170,10,85,30);
-	 penSub.setBounds(10,0,245,30);
-	 penSub.setBorder(BorderFactory.createLineBorder(Color.BLUE,1));
-	 penScientific.add(penSub);
-	 this.add(penScientific);
-	 penScientific.setBounds(0, 80,550, 300);
-	 // penScientific.setBackground(Color.GREEN);
-	 btnBut[5][4].setVisible(false);
-	 btnBut[5][1].setVisible(false);
-	 btnBut[4][4].setSize(w, h + h + d);
-	 btnBut[5][0].setSize(w + w + d, h);
-	 mniExit.addActionListener(new ActionListener() {
-	
-	 @Override
-	 public void actionPerformed(ActionEvent arg0) {
-	 // TODO Auto-generated method stub
-	 System.exit(0);
-	 }
-	 });
-	 mniExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
-	
-	 }
 
+	// FIXME chinh loi cua boder bg
+	public void MenuMinhScientific() {
+		txtSrc2 = new JTextField();
+		penScientific.add(txtSrc2);
+		txtSrc2.setBounds(10, 20, 500, 60);
+		txtSrc2.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+		txtSrc2.setHorizontalAlignment(JTextField.RIGHT);
+		txtSrc2.setEnabled(false);
+		mnBar = new JMenuBar();
+		mneView = new JMenu("View");
+		mneEdit = new JMenu("Edit");
+		mneHelp = new JMenu("Help");
+		mniStan = new JMenuItem("Standard");
+		mniScien = new JMenuItem("Scientific");
+		mniPro = new JMenuItem("Programmer");
+		mniExit = new JMenuItem("Exit");
+		mneView.add(mniStan);
+		mneView.addSeparator();
+		mneView.add(mniScien);
+		mneView.addSeparator();
+		mneView.add(mniPro);
+		mneView.addSeparator();
+		mneView.add(mniExit);
+		mnBar.add(mneView);
+		mnBar.add(mneEdit);
+		mnBar.add(mneHelp);
+		setJMenuBar(mnBar);
+		mneView.setMnemonic(KeyEvent.VK_V);
+		mneEdit.setMnemonic(KeyEvent.VK_E);
+		mneHelp.setMnemonic(KeyEvent.VK_H);
+		penScientific.setLayout(null);
+		penSub.setLayout(null);
+	
+		Insets s = new Insets(1, 1, 1, 1);
+		for (int i = 0; i < 6; i++) {
+			x2 = 260;
+			for (int j = 0; j < 5; j++) {
+				btnBut[i][j] = new JButton(btn[i][j]);
+				penScientific.add(btnBut[i][j]);
+				btnBut[i][j].setBounds(x2, y2, w, h);
+				btnBut[i][j].setMargin(s);
+
+				x2 = x2 + d + w;
+			}
+			y2 = y2 + d + w;
+		}
+		y2 = w + d;
+		y2 = 150;
+		for (int i = 0; i < 5; i++) {
+			x2 = 10;
+			for (int j = 0; j < 5; j++) {
+				btnBut2[i][j] = new JButton(btn2[i][j]);
+				penScientific.add(btnBut2[i][j]);
+				btnBut2[i][j].setBounds(x2, y2, w, h);
+				btnBut2[i][j].setMargin(s);
+				x2 = x2 + w + d;
+			}
+			y2 = y2 + d + w;
+		}
+		bgDeg = new JRadioButton("Degrees");
+		bgRa = new JRadioButton("Radians");
+		bgGra = new JRadioButton("Grads");
+		bgSelect.add(bgDeg);
+		bgSelect.add(bgGra);
+		bgSelect.add(bgRa);
+		penSub.add(bgDeg);
+		penSub.add(bgRa);
+		penSub.add(bgGra);
+		bgDeg.setBounds(5, 10, 75, 30);
+		bgRa.setBounds(80, 10, 75, 30);
+		bgGra.setBounds(160,10, 80, 30);
+		penSub.setBounds(10,100, 245,45);
+		penSub.setBorder(BorderFactory.createLineBorder(Color.BLUE, 1));
+		penScientific.add(penSub);
+		this.add(penScientific);
+		penScientific.setBounds(0,0, 550, 500);
+		// penScientific.setBackground(Color.GREEN);
+		btnBut[5][4].setVisible(false);
+		btnBut[5][1].setVisible(false);
+		btnBut[4][4].setSize(w, h + h + d);
+		btnBut[5][0].setSize(w + w + d, h);
+		mniExit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}
+		});
+		mniExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
+
+	}
+	public void MenuProGrammer(){
+		txtSrc3 = new JTextField();
+		penProGrammer.add(txtSrc3);
+		txtSrc3.setBounds(10, 20, 500, 60);
+		txtSrc3.setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+		txtSrc3.setHorizontalAlignment(JTextField.RIGHT);
+		txtSrc3.setEnabled(false);
+		Font f = txtSrc3.getFont();
+		txtSrc3.setFont(new Font(f.getName(), Font.BOLD, f.getSize() + 20));
+		mnBar = new JMenuBar();
+		mneView = new JMenu("View");
+		mneEdit = new JMenu("Edit");
+		mneHelp = new JMenu("Help");
+		mniStan = new JMenuItem("Standard");
+		mniScien = new JMenuItem("Scientific");
+		mniPro = new JMenuItem("Programmer");
+		mniExit = new JMenuItem("Exit");
+		mneView.add(mniStan);
+		mneView.addSeparator();
+		mneView.add(mniScien);
+		mneView.addSeparator();
+		mneView.add(mniPro);
+		mneView.addSeparator();
+		mneView.add(mniExit);
+		mnBar.add(mneView);
+		mnBar.add(mneEdit);
+		mnBar.add(mneHelp);
+		setJMenuBar(mnBar);
+		mneView.setMnemonic(KeyEvent.VK_V);
+		mneEdit.setMnemonic(KeyEvent.VK_E);
+		mneHelp.setMnemonic(KeyEvent.VK_H);
+		penProGrammer.setLayout(null);
+		Insets s = new Insets(1, 1, 1, 1);
+
+		for (int i = 0; i < 6; i++) {
+			x3 = 260;
+			for (int j = 0; j < 5; j++) {
+				btnBut3[i][j] = new JButton(btn[i][j]);
+				penProGrammer.add(btnBut[i][j]);
+				btnBut3[i][j].setBounds(x3, y3, w, h);
+				btnBut3[i][j].setMargin(s);
+
+				x3 = x3 + d + w;
+			}
+			y3 = y3 + d + w;
+		}
+		y3 = w + d;
+		this.add(penProGrammer);
+		penProGrammer.setBounds(0,0, 550, 500);
+		btnBut3[5][4].setVisible(false);
+		btnBut3[5][1].setVisible(false);
+		btnBut3[4][4].setSize(w, h + h + d);
+		btnBut3[5][0].setSize(w + w + d, h);
+		mniExit.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}
+		});
+		mniExit.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_L, ActionEvent.CTRL_MASK));
+	}
 	public void ActionBtnMinhStandard() {
 		ActionListener bactUoin = new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent g) {
-				
-				JButton btnReply = (JButton)g.getSource();
-				String bNumber =btnReply.getText();
+
+				JButton btnReply = (JButton) g.getSource();
+				String bNumber = btnReply.getText();
 				String bCurrent = txtSrc.getText();
-				if(bCurrent.equals("0")){
+				if (bCurrent.equals("0")) {
 					txtSrc.setText(bNumber);
-				}else {
-					txtSrc.setText(bCurrent+bNumber);
+				} else {
+					txtSrc.setText(bCurrent + bNumber);
 				}
 			}
 		};
-		//FIXME xem lai số 0
+		// FIXME xem lai số 0
 		for (int i = 2; i < 5; i++) {
 			for (int j = 0; j < 3; j++) {
 				btnBut[i][j].addActionListener(bactUoin);
@@ -241,35 +342,35 @@ public class Calculator extends JFrame {
 		}
 		btnBut[5][0].addActionListener(bactUoin);
 		ActionListener bAction = new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent g) {
 				// TODO Auto-generated method stub
-				if(g.getSource()==mniStan){
+				if (g.getSource() == mniStan) {
 					penStandard.setVisible(true);
 					penScientific.setVisible(false);
-				}else if(g.getSource()==mniScien){
+				} else if (g.getSource() == mniScien) {
 					penScientific.setVisible(true);
 					penStandard.setVisible(false);
-				}else if(g.getSource()==btnBut[5][3]){
+				} else if (g.getSource() == btnBut[5][3]) {
 					Plus();
-				}else if(g.getSource()==btnBut[4][3]){
+				} else if (g.getSource() == btnBut[4][3]) {
 					Minus();
-				}else if(g.getSource()==btnBut[3][3]){
+				} else if (g.getSource() == btnBut[3][3]) {
 					Multi();
-				}else if(g.getSource()==btnBut[2][3]){
+				} else if (g.getSource() == btnBut[2][3]) {
 					division();
-				}else if(g.getSource()==btnBut[4][4]){
+				} else if (g.getSource() == btnBut[4][4]) {
 					EqualMinh();
-				}else if(g.getSource()==btnBut[1][1]){
+				} else if (g.getSource() == btnBut[1][1]) {
 					butCE();
-				}else if(g.getSource()==btnBut[0][2]){
+				} else if (g.getSource() == btnBut[0][2]) {
 					bMS();
-				}else if(g.getSource()==btnBut[0][1]){
+				} else if (g.getSource() == btnBut[0][1]) {
 					bMR();
-				}else if(g.getSource()==btnBut[0][0]){
+				} else if (g.getSource() == btnBut[0][0]) {
 					bMC();
-				}else if(g.getSource()==btnBut[1][0]){
+				} else if (g.getSource() == btnBut[1][0]) {
 					bDelete();
 				}
 			}
@@ -282,6 +383,7 @@ public class Calculator extends JFrame {
 		mniScien.addActionListener(bAction);
 		mniStan.addActionListener(bAction);
 	}
+
 	public void Plus() {
 		String Number1 = txtSrc.getText();
 		txtSrc.setText("");
@@ -329,33 +431,36 @@ public class Calculator extends JFrame {
 			txtSrc.setText(NumberChange / NumberChange2 + "");
 		}
 	}
-public void butCE(){
-	txtSrc.setText("");
-}
-public void bMS(){
-	String a;
-	a=txtSrc.getText();
-	MS=Integer.parseInt(a);
-}
-public void bMR(){
-	txtSrc.setText(MS+"");
-}
-public void bMC(){
-	MS=0;
-}
-public void bDelete(){
-	String a= txtSrc.getText();
-	a=a.substring(0,a.length()-1);
-	txtSrc.setText(Integer.parseInt(a)+"");
-}
- 
 
+	public void butCE() {
+		txtSrc.setText("");
+	}
+
+	public void bMS() {
+		String a;
+		a = txtSrc.getText();
+		MS = Integer.parseInt(a);
+	}
+
+	public void bMR() {
+		txtSrc.setText(MS + "");
+	}
+
+	public void bMC() {
+		MS = 0;
+	}
+
+	public void bDelete() {
+		String a = txtSrc.getText();
+		a = a.substring(0, a.length() - 1);
+		txtSrc.setText(Integer.parseInt(a) + "");
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Calculator maindown =new Calculator();
+		Calculator maindown = new Calculator();
 		maindown.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		maindown.setVisible(true);
 	}
